a7585860ecbf2c5a240bdd130a483d0517f2a9d7
Urth Tan <ruisiang96@gmail.com>
2021-09-05 11:28:38 +0800

    adding vpc and routing table, association

---
+++ vpc.tf
@@ -0,0 +1,52 @@
+#
+# VPC Resources
+#  * VPC
+#  * Subnets
+#  * Internet Gateway
+#  * Route Table
+#
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = tomap(
+    {
+      "Name"                                      = "codepipeline-k8-terraform-node"
+      "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    }
+  )
+}
+
+resource "aws_subnet" "my_vpc_subnets" {
+  count                   = 2
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  cidr_block              = "10.0.${count.index}.0/24"
+  map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.my_vpc.id
+  tags = tomap(
+    {
+      "Name"                                      = "codepipeline-k8-terraform-node"
+      "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    }
+  )
+}
+
+resource "aws_internet_gateway" "my_internet_gateway" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "codepipeline-k8-terraform"
+  }
+}
+
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_internet_gateway.id
+  }
+}
+
+resource "aws_route_table_association" "my_route_table_association" {
+  count          = 2
+  subnet_id      = aws_subnet.my_vpc_subnets.*.id[count.index]
+  route_table_id = aws_route_table.my_route_table.id
+}
